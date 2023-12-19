85cd14fa612de6109b9dfe389cd9a21783c42f34
Jose Funez <joseafp@algartech.com>
2023-12-19 15:29:41 -0500

    Ajuste mensaje clase de excepcion

--- src/main/java/com/app/mycityweatherapp/exception/CityNotFoundException.java
+++ src/main/java/com/app/mycityweatherapp/exception/CityNotFoundException.java
@@ -3,6 +3,7 @@ package com.app.mycityweatherapp.exception;
 public class CityNotFoundException extends RuntimeException {
 
     public CityNotFoundException(String cityName,String countryCode) {
-        super("No se pudo encontrar la ciudad:" + cityName + ", " + countryCode);
+        super("No se pudo encontrar la ciudad: " + (cityName!=null ? cityName+ ", ":"")
+                + (countryCode!=null?countryCode:""));
     }
 }
