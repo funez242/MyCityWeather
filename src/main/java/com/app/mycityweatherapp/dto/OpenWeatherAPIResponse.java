24f7de6bdaec450b1487cfb1e9eaa404a0161bf7
Jose Funez <joseafp@algartech.com>
2023-12-19 03:57:18 -0500

    Primera implementación de la clase de servicio.

---
+++ src/main/java/com/app/mycityweatherapp/dto/OpenWeatherAPIResponse.java
@@ -0,0 +1,20 @@
+package com.app.mycityweatherapp.dto;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.io.Serializable;
+import java.util.List;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@JsonIgnoreProperties(ignoreUnknown = true)
+public class OpenWeatherAPIResponse implements Serializable {
+
+    List<Weather> weather;
+
+    Main main;
+}
---
+++ src/main/java/com/app/mycityweatherapp/service/WeatherService.java
@@ -0,0 +1,64 @@
+package com.app.mycityweatherapp.service;
+
+import com.app.mycityweatherapp.dto.CityDetails;
+
+import com.app.mycityweatherapp.dto.OpenWeatherAPIResponse;
+import com.app.mycityweatherapp.util.Constants;
+import org.springframework.boot.web.client.RestTemplateBuilder;
+
+import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestTemplate;
+
+import java.util.Arrays;
+import java.util.List;
+
+
+@Service
+public class WeatherService {
+
+    private final RestTemplate restTemplate;
+
+    public WeatherService(RestTemplateBuilder restTemplateBuilder) {
+        this.restTemplate = restTemplateBuilder.build();
+    }
+
+
+    public OpenWeatherAPIResponse getWeatherByCityName(String cityName, String countryCode){
+        String geoURL = getOpenWeatherGeoUrl(cityName,countryCode);
+
+        CityDetails[] geoResponse = this.restTemplate.getForObject(geoURL,CityDetails[].class);
+        System.out.println("Ejecuccion geo correcta");
+        List<CityDetails> cityDetailsList= Arrays.asList(geoResponse);
+
+        String latitude = cityDetailsList.get(0).getLatitude();
+        String longitude = cityDetailsList.get(0).getLongitude();
+
+        String weatherURL = getOpenWeatherUrl(latitude,longitude);
+
+        OpenWeatherAPIResponse weatherAPIResponse = this.restTemplate.getForObject(weatherURL, OpenWeatherAPIResponse.class);
+
+        return weatherAPIResponse;
+    }
+
+
+    private String getOpenWeatherGeoUrl(String cityName, String countryCode) {
+        cityName = cityName+","+countryCode;
+        return Constants.OPENWEATHER_API_GEO_BASE_URL +
+                Constants.OPENWEATHER_API_CITYNAME +
+                cityName +
+                Constants.OPENWEATHER_API_LIMIT+
+                Constants.OPENWEATHER_API_KEY +
+                Constants.API_KEY;
+    }
+
+    private String getOpenWeatherUrl(String latitude, String longitude) {
+        return Constants.OPENWEATHER_API_BASE_URL +
+                Constants.OPENWEATHER_API_LAT + latitude +
+                Constants.OPENWEATHER_API_LON + longitude +
+                Constants.OPENWEATHER_API_LANG +
+                Constants.OPENWEATHER_API_UNITS +
+                Constants.OPENWEATHER_API_KEY +
+                Constants.API_KEY;
+    }
+
+}
