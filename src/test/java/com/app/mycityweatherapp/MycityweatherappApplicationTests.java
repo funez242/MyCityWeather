b4ef2b264d29de224a5f651db9e7a5fc8411db7a
Jose Antonio Funez Perez <46043946@comcel.com.co>
2023-12-18 22:00:49 -0500

    Fase inicial del proyecto My City - Weather App

---
+++ src/test/java/com/app/mycityweatherapp/MycityweatherappApplicationTests.java
@@ -0,0 +1,13 @@
+package com.app.mycityweatherapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MycityweatherappApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
